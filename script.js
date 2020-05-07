c4490cfc3164a5399b30ed919166f0cc38a37768
satanrout <satyaranjanrout81@gmail.com>
2020-05-07 11:50:26 +0530

    a interactive mobile site for kids to learn in a fun way

---
+++ script.js
@@ -0,0 +1,127 @@
+const lionInfo = document.getElementById("lion-info");
+const aligatorSound = document.getElementById("aligator-sound")
+const bearSound = document.getElementById("bear-sound")
+const catSound = document.getElementById("cat-sound")
+const dogSound = document.getElementById("dog-sound")
+const elephantSound = document.getElementById("elephant-sound")
+const frogSound = document.getElementById("frog-sound")
+const goatSound = document.getElementById("goat-sound")
+const horseSound = document.getElementById("horse-sound")
+const iguanaSound = document.getElementById("iguana-sound")
+const jellyfishSound = document.getElementById("jellyfish-sound")
+const koalaSound = document.getElementById("koala-sound")
+const lionSound = document.getElementById("lion-sound")
+const monkeySound = document.getElementById("monkey-sound")
+const nilgaiSound = document.getElementById("nilgai-sound")
+const octopusSound = document.getElementById("octopus-sound")
+const penguinSound = document.getElementById("penguin-sound")
+const rabbitSound = document.getElementById("rabbit-sound")
+const snakeSound = document.getElementById("snake-sound")
+const tigerSound = document.getElementById("tiger-sound")
+const unicornSound = document.getElementById("unicorn-sound")
+const vultureSound = document.getElementById("vulture-sound")
+const whaleSound = document.getElementById("whale-sound")
+const yakSound = document.getElementById("yak-sound")
+const zebraSound = document.getElementById("zebra-sound")
+
+
+lionInfo.addEventListener('click', function(){
+	const test = document.querySelector('.test');
+	test.play();
+});
+aligatorSound.addEventListener('click', function(){
+	const aligator = document.querySelector('.aligator');
+	aligator.play();
+});
+bearSound.addEventListener('click', function(){
+	const bear = document.querySelector('.bear');
+	bear.play();
+});
+catSound.addEventListener('click', function(){
+	const cat = document.querySelector('.cat');
+	cat.play();
+});
+dogSound.addEventListener('click', function(){
+	const dog = document.querySelector('.dog');
+	dog.play();
+});
+elephantSound.addEventListener('click', function(){
+	const elephant = document.querySelector('.elephant');
+	elephant.play();
+});
+frogSound.addEventListener('click', function(){
+	const frog = document.querySelector('.frog');
+	frog.play();
+});
+goatSound.addEventListener('click', function(){
+	const goat = document.querySelector('.goat');
+	goat.play();
+});
+horseSound.addEventListener('click', function(){
+	const horse = document.querySelector('.horse');
+	horse.play();
+});
+iguanaSound.addEventListener('click', function(){
+	const iguana = document.querySelector('.iguana');
+	iguana.play();
+});
+jellyfishSound.addEventListener('click', function(){
+	const jellyfish = document.querySelector('.jellyfish');
+	jellyfish.play();
+});
+koalaSound.addEventListener('click', function(){
+	const koala = document.querySelector('.koala');
+	koala.play();
+});
+lionSound.addEventListener('click', function(){
+	const lion = document.querySelector('.lion');
+	lion.play();
+});
+monkeySound.addEventListener('click', function(){
+	const monkey = document.querySelector('.monkey');
+	monkey.play();
+});
+nilgaiSound.addEventListener('click', function(){
+	const nilgai = document.querySelector('.nilgai');
+	nilgai.play();
+});
+octopusSound.addEventListener('click', function(){
+	const octopus = document.querySelector('.octopus');
+	octopus.play();
+});
+penguinSound.addEventListener('click', function(){
+	const penguin = document.querySelector('.penguin');
+	penguin.play();
+});
+rabbitSound.addEventListener('click', function(){
+	const rabbit = document.querySelector('.rabbit');
+	rabbit.play();
+});
+snakeSound.addEventListener('click', function(){
+	const snake = document.querySelector('.snake');
+	snake.play();
+});
+tigerSound.addEventListener('click', function(){
+	const tiger = document.querySelector('.tiger');
+	tiger.play();
+});
+unicornSound.addEventListener('click', function(){
+	const unicorn = document.querySelector('.unicorn');
+	unicorn.play();
+});
+vultureSound.addEventListener('click', function(){
+	const vulture = document.querySelector('.vulture');
+	vulture.play();
+});
+whaleSound.addEventListener('click', function(){
+	const whale = document.querySelector('.whale');
+	whale.play();
+});
+yakSound.addEventListener('click', function(){
+	const yak = document.querySelector('.yak');
+	yak.play();
+});
+zebraSound.addEventListener('click', function(){
+	const zebra = document.querySelector('.zebra');
+	zebra.play();
+});
